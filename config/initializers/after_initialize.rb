cef64cb3e55f708fd728941ab05dbfba73b7e6a5
kota.sugihara <kou.cshu@gmail.com>
2014-11-30 15:37:15 +0900

    heroku deploy[4]

--- config/initializers/after_initialize.rb
+++ config/initializers/after_initialize.rb
@@ -5,7 +5,7 @@ Rails.application.config.after_initialize do
     if Rails.application.config.database_configuration
       config = Rails.application.config.database_configuration[Rails.env]
       config['reaping_frequency'] = ENV['DB_REAP_FREQ'] || 10 # seconds
-      config['pool']              = ENV['DB_POOL']      || 5
+      config['pool'] = ENV['DB_POOL'] || 5
       ActiveRecord::Base.establish_connection(config)
     end
   end
