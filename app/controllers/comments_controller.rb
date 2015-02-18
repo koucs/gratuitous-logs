68e4400d738e21b81996734e3cc72b7d648e1f29
kota.sugihara <kou.cshu@gmail.com>
2015-02-19 00:57:21 +0900

    Comment Function(List, Add comment, Delete coment)

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -7,6 +7,15 @@ class CommentsController < ApplicationController
         redirect_to post_path(@post)
     end
 
+    # DELETE /posts/:post_id/comments/:id
+    def destroy
+        @post = Post.find(params[:post_id])
+        @comment = @post.comments.find(params[:id])
+        @comment.destroy
+
+        redirect_to post_path(@post)
+    end
+
     private
         def comment_params
             params.require(:comment).permit(:commentator, :body)
