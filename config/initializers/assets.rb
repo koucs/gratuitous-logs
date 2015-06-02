f25d499eaff59fe7db4acb3a00b359864b60c697
kota.sugihara <kou.cshu@gmail.com>
2015-06-03 00:21:08 +0900

    Redesign Top Page(2)
    $B;29M!'(Bfullscreen overlay styles & effects http://tympanus.net/Development/FullscreenOverlayStyles/index3.html
    $B%H%C%W%Z!<%8$N%a%K%e!<%\%?%s$K%U%k%9%/%j!<%s%a%K%e!<%"%K%a!<%7%g%s5!G=$rDI2C(B
    
    Next -> $B5-;v%Z!<%8$N%G%6%$%s!"5-;v0lMw2hA|!"(BSNS$B%\%?%s$J$I(B

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -8,3 +8,7 @@ Rails.application.config.assets.version = '1.0'
 # Rails.application.config.assets.precompile += %w( search.js )
 Rails.application.config.assets.precompile += %w( posts.css )
 Rails.application.config.assets.precompile += %w( posts.js )
+Rails.application.config.assets.precompile += %w( home.css )
+Rails.application.config.assets.precompile += %w( home.js )
+Rails.application.config.assets.precompile += %w( classie.js )
+Rails.application.config.assets.precompile += %w( fullscreen-demo1.js )
