d75911617b46f3ff4737b1b5b26b786e8b7aa7be
kota.sugihara <kou.cshu@gmail.com>
2016-02-26 15:18:25 +0900

    Bug Fix

---
+++ spec/views/articles/show.html.erb_spec.rb
@@ -0,0 +1,21 @@
+require 'rails_helper'
+
+RSpec.describe "articles/show", :type => :view do  context "no data" do
+    subject { render }
+    before :each do
+        category = create :category
+        @tags = Post.tag_counts_on(:tags)
+        @post = create :valid_no_draft_post, category: category, tag_list: "TEST_TAG"
+    end
+
+    it "being displayed 'Title' " do
+        should match(@post.Title)
+    end
+
+    it "being displayed 'Tag Cloud' " do
+        should match(/TEST_TAG/)
+    end
+  end
+
+end
+
