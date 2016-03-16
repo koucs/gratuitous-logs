aac2e0dee1880b5f9705167978da281b57ab781f
kota.sugihara <kou.cshu@gmail.com>
2016-03-16 23:41:57 +0900

    [Func] Adds Editing Mathematical formula and update bundle
    - Mathematical formula via Mathjax
    - bundle update

--- app/helpers/syntax_helper.rb
+++ app/helpers/syntax_helper.rb
@@ -8,16 +8,37 @@ module SyntaxHelper
       super extensions.merge(link_attributes: { target: "_blank" } )
     end
 
+    # def block_code(code, language)
+    #   sha = Digest::SHA1.hexdigest(code)
+
+    #   # fetchは code-[language]-[sha]がキャッシュに保存されていた場合
+    #   Rails.cache.fetch ["code", language, sha].join('-') do
+    #     Pygments.highlight(code, lexer: language, options: {class: "code", linespans: "line"})
+    #   end
+    # end
+
     def block_code(code, language)
       sha = Digest::SHA1.hexdigest(code)
-
-      # fetchは code-[language]-[sha]がキャッシュに保存されていた場合は
+      # fetchは code-[language]-[sha]がキャッシュに保存されていた場合
       Rails.cache.fetch ["code", language, sha].join('-') do
+        if language=="mathjax"
+          code = "<script type=\"math/tex; mode=display\">\n#{code}\n</script>"
+        end
         Pygments.highlight(code, lexer: language, options: {class: "code", linespans: "line"})
       end
     end
 
+  def codespan(code)
+    if code[0] == "$" && code[-1] == "$"
+      "<script type=\"math/tex\">#{code[1...-1]}</script>"
+    elsif code[0..1] == "\\(" && code[-2..-1] == "\\)"
+      "<script type=\"math/tex\">#{code[2...-2]}</script>"
+    else
+      "<code>#{code}</code>"
+    end
   end
+
+end
 
   def markdown(text)
     renderer = HTMLwithPygments.new(hard_wrap: true, filter_html: false)
